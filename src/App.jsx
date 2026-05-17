2bf4ed971fd76cda68ed683f2816b48edb34c6e0
shahriar1100 <sahariarsikder.ss@gmail.com>
2026-05-17 12:15:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <section id="center">
+
+        <div>
+          <h1>Get started</h1>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+
+
+
+    </>
+  )
+}
+
+export default App
