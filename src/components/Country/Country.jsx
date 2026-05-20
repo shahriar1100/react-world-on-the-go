d83f4659499e9b9e39c626b26277ffe4c52d81f3
shahriar1100 <sahariarsikder.ss@gmail.com>
2026-05-20 11:40:13 +0600

    Add visited country card gradient active style

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,23 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Country({ country }) {
+  
+  const [visited, setVisited] = useState(false)
+  
+  const handleVisited = () => {
+    // if(visited){
+    //   setVisited(false)
+    // }
+    // else{
+    //   setVisited(true)
+    // }
 
-    const handleVisited = ()=>{
-      console.log("btn clicked");
-      
-    }
+    // setVisited(visited ? false : true)
+
+    setVisited(!visited)
+  };
 
   return (
     <>
-      <div className="countryCard mainCard">
+      <div className={`countryCard mainCard ${visited && "countryVisited"}`}>
         <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
         <h3 className="textBlack">
           Name: <span className="countryName"> {country.name.common}</span>
         </h3>
         <p className="textBlack">Population: {country.population.population}</p>
         <p className="textBlack">Region: {country.region.region}</p>
-        <p className="textBlack">Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
-        <button className="btn" onClick={handleVisited}>Not Visited</button>
+        <p className="textBlack">
+          Area: {country.area.area}{" "}
+          {country.area.area > 300000 ? "Big Country" : "Small Country"}
+        </p>
+        <button className="btn" onClick={handleVisited}>
+          {visited ? 'Visited':'Not Visited'}
+        </button>
       </div>
     </>
   );
